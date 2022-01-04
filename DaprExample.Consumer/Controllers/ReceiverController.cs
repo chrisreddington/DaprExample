c8252bbb973e794d229b5bd737b0288271c1d975
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2022-01-04 15:18:49 +0000

    Initial commit

---
+++ DaprExample.Consumer/Controllers/ReceiverController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DaprExample.Consumer.Models;
+using Dapr;
+using Dapr.Client;
+
+namespace DaprExample.Consumer.Controllers;
+
+[ApiController]
+public class ReceiverController : Controller
+{
+    [HttpPost("/azurebusbinding")]
+    public ActionResult<string> getCheckout([FromBody] int orderId)
+    {
+        Console.WriteLine("Received Message: " + orderId);
+        return Ok("CID" + orderId);
+    }
+}
---
+++ DaprExample.Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿// Use the Dapr Client .NET SDK
+using Dapr.Client;
+
+// Set up the appropriate binding information
+string BINDING_NAME = "azurebusbinding";
+string BINDING_OPERATION = "create";
+
+// Initialise the counter to 0 and init the Dapr Client
+var daprClient = new DaprClientBuilder().Build();
+var counter = 0;
+
+// Keep looping every second, create a message by invoking the binding.
+while (true)
+{
+    Console.WriteLine($"Counter = {counter++}");
+    await daprClient.InvokeBindingAsync(BINDING_NAME, BINDING_OPERATION, counter);
+    await Task.Delay(1000);
+}
